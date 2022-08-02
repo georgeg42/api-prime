7d4bae2a28bb57746e2515388fea6f43b17a3efa
georgeg42 <georgesilva4295@gmail.com>
2022-08-02 18:52:51 -0300

    cadastro

---
+++ cadastro/cadastro/src/main/java/com/primeit/digitalbank/api/cadastro/model/Profissões.java
@@ -0,0 +1,12 @@
+package com.primeit.digitalbank.api.cadastro.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+public class Profissões {
+
+}
